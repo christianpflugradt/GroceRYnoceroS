0af2e4a9cb24d8eb4bbc0170b547ec38573110dc
Christian Pflugradt <christian@pflugradts.de>
2020-09-28 19:31:40 +0200

    added subroutine:  manage groceries

---
+++ subroutines/manage_groceries.rb
@@ -0,0 +1,33 @@
+module ManageGroceries
+  extend self, Menu
+
+  @subroutines = [
+      :enter_create_groceries_sub,
+      :enter_assign_groceries_sub,
+      :enter_view_groceries_sub
+  ]
+
+  @menu = <<MENU
+
+  [Manage Groceries]
+
+  (1) create new groceries
+  (2) assign groceries to categories
+  (3) view, rename or delete groceries
+  (4) return to previous menu
+
+MENU
+
+  def enter_create_groceries_sub(db)
+    puts "not yet implemented :("
+  end
+
+  def enter_assign_groceries_sub(db)
+    puts "not yet implemented :("
+  end
+
+  def enter_view_groceries_sub(db)
+    puts "not yet implemented :("
+  end
+
+end
