f54e2622bb5dd5c2e5f078abe81bd5448e246a22
Christian Pflugradt <christian@pflugradts.de>
2020-10-04 11:04:09 +0200

    added subroutine for creating new categories

---
+++ common/database/category_sql.rb
@@ -0,0 +1,19 @@
+module SqlCategory
+
+  def insert_category(category)
+    @db.execute "INSERT INTO categories (name, created_at) VALUES (?, datetime('now'))", category.strip
+  end
+
+  def delete_categories(ids)
+    @db.execute "DELETE FROM categories WHERE id IN (#{ids.map(&:to_s).join(',')})"
+  end
+
+  def rename_categories(id, new_name)
+    @db.execute 'UPDATE categories SET name = ? WHERE id = ?', new_name, id
+  end
+
+  def select_categories(filter)
+    @db.query 'SELECT id, name FROM categories WHERE name like ?', "#{filter}%"
+  end
+
+end
